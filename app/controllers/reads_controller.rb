533774ce4d57baa41932ff13df437ff775d42018
Dan Broadbent <dbroadbent85@gmail.com>
2016-12-19 18:22:40 -0700

    User can see reads

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -1,7 +1,12 @@
 class ReadsController < ApplicationController
+  def index
+    @reads = Read.all
+  end
+
   def create
     read = Read.create(read_params)
   end
+
   private
   def read_params
     params.require(:read).permit(:title, :url)
